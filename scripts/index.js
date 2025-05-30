2a2db444b6a698fe98abb4e53125fd07bf8d42ef
Dede <dedenyengor09@gmail.com>
2025-05-30 02:14:20 +0000

    My first commit

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+// declare variable to store navbar element
+const navElement = document.getElementById("navbar")
+// detect scroll evnet on document
+document.addEventListener('scroll',function() {
+    // console.log(window.scrollY);
+    if (window.scrollY > 150) {
+        // change nav bar bg to blue
+        navElement.style.backgroundColor = "pink";
+    }
+    else {
+        // change nav bar to transparent
+        navElement.style.backgroundColor = "rgba(255,255,255,0.2)";
+    }
+});
+
+
+function goToSite() {
+      window.location.href = "https://example.com"; 
+    }
+
+// new Typed("#typed",{
+//     strings: ["Frontend Developer", "Photographer"],
+//     typeSpeed: 60,
+//     backSpeed: 30,
+//     loop: true,
+// });
